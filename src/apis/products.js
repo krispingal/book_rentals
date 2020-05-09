89044fd6ee4ee2f59979a63613afdfd94264d4fe
Krishna <krishna.pingal@gmail.com>
2020-05-08 23:00:52 -0700

    Add links to apis

---
+++ src/apis/products.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+	baseURL: 'https://20200508t195146-dot-productservice-dot-studentrentals.uc.r.appspot.com'
+})
---
+++ src/apis/profiles.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+	baseURL: 'https://20200508t221254-dot-profileservice-dot-studentrentals.uc.r.appspot.com/'
+})
